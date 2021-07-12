9523f66a2926d8e2e2c2fb37d8d2f2dc2c53f486
denis-shvets <denis.shvets.a+github.com@gmail.com>
2021-07-12 20:33:21 +0000

    Deploying to main from @ denis-shvets/denis-shvets.github.io@2a161ee5fd98ea962321648cad18f635a452e5bd 🚀

---
+++ _next/static/nCEaCfwSE8D4UdyIxF4lw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8818907fd4fe7cb97f23.css","static/chunks/pages/index-267d5b6ceb1954319085.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
